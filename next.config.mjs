0e20626fbceb4d8715f5e892aa6ab25a034e4c58
Yenegh Badimayalew <your@email.com>
2026-04-08 10:28:57 +1200

    Fix GitHub Pages asset paths

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,14 @@
 /** @type {import('next').NextConfig} */
+const isProd = process.env.NODE_ENV === "production";
+
 const nextConfig = {
   output: "export",
   images: {
     unoptimized: true,
   },
   trailingSlash: true,
+  basePath: isProd ? "/yenegh-portfolio" : "",
+  assetPrefix: isProd ? "/yenegh-portfolio/" : "",
 };
 
 export default nextConfig;
