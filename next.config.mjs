5448763b992229becff72493575beac5160e1bec
Yenegh Badimayalew <your@email.com>
2026-04-08 11:01:01 +1200

    Remove repo basePath for custom domain

--- next.config.mjs
+++ next.config.mjs
@@ -1,14 +1,10 @@
 /** @type {import('next').NextConfig} */
-const isProd = process.env.NODE_ENV === "production";
-
 const nextConfig = {
   output: "export",
   images: {
     unoptimized: true,
   },
   trailingSlash: true,
-  basePath: isProd ? "/yenegh-portfolio" : "",
-  assetPrefix: isProd ? "/yenegh-portfolio/" : "",
 };
 
 export default nextConfig;
