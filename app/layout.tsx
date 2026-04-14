a9067639ad4aeaf7c71a3f0baede9def42b77abc
Yenegh <yenegh@gmail.com>
2026-04-14 13:50:41 +1200

    Add Google Analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from "next";
+import Script from "next/script";
 import "./globals.css";
 import { SiteChrome } from "@/components/SiteChrome";
 
@@ -13,6 +14,19 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body>
+        <Script
+          src="https://www.googletagmanager.com/gtag/js?id=G-S45QSE1BJ4"
+          strategy="afterInteractive"
+        />
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-S45QSE1BJ4');
+          `}
+        </Script>
+
         <SiteChrome>{children}</SiteChrome>
       </body>
     </html>
